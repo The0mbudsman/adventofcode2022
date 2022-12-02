9788ab707282b98f0004ebab3781ff9c3a208058
The0mbudsman <37667514+The0mbudsman@users.noreply.github.com>
2022-12-02 18:30:25 +0000

    Add files via upload

---
+++ day1/day1.mjs
@@ -0,0 +1,5 @@
+import fs from 'fs';
+const data = fs.readFileSync("input.txt").toString().split("\n\n").map(line => line.split("\n"))
+const elves = data.map(l => l.reduce((a,b) => parseInt(a)+parseInt(b),0))
+elves.sort((a,b) => {return b - a})
+console.log(elves[0])
