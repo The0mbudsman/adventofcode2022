390aedcb6c25981e0eb4f8c9b47711fe3aac29d4
The0mbudsman <37667514+The0mbudsman@users.noreply.github.com>
2022-12-02 18:38:19 +0000

    Update day1.mjs

--- day1/day1.mjs
+++ day1/day1.mjs
@@ -3,3 +3,4 @@ const data = fs.readFileSync("input.txt").toString().split("\n\n").map(line => l
 const elves = data.map(l => l.reduce((a,b) => parseInt(a)+parseInt(b),0))
 elves.sort((a,b) => {return b - a})
 console.log(elves[0])
+console.log(elves[0]+elves[1]+elves[2])
