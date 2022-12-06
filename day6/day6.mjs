1824fbc8e7c3de6e16308f7ba6e01788934fa59d
The0mbudsman <37667514+The0mbudsman@users.noreply.github.com>
2022-12-06 10:46:52 +0000

    Update day6.mjs

--- day6/day6.mjs
+++ day6/day6.mjs
@@ -1,11 +1,12 @@
 import fs from 'fs';
 const data = fs.readFileSync("./key.txt").toString();
-const size = 4
-for (var i = 0 ; i < [...data].length; i++) {
-    const window = data.slice(i, i+size)
-    if (window.length == [...new Set(window)].length){
-        console.log(i+size)
-        console.log(window)
-        break;
+for (const size of [4,14]){
+    for (var i = 0 ; i < [...data].length; i++) {
+        const window = data.slice(i, i+size)
+        if (window.length == [...new Set(window)].length){
+            console.log(i+size)
+            console.log(window)
+            break;
+        }
     }
 }
